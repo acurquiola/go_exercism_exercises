4291e6feafd18aa55e847aece1bdf2f7100a4fa2
Ana U <anitaurquiola28@gmail.com>
2021-07-02 19:03:18 -0300

    lunhn

--- luhn/luhn.go
+++ luhn/luhn.go
@@ -1,52 +1,39 @@
 package luhn
 
 import (
-	"fmt"
-	"log"
-	"regexp"
+	"strconv"
+	"strings"
 )
 
 func Valid(input string) bool {
 	status := false
+	input = strings.ReplaceAll(input, " ", "")
+
 	if len(input) > 1 {
-		stringResult := ""
-		fmt.Println(input, "new")
-		inputNew := getOnlyNumeric(input)
-		fmt.Println(inputNew, "inputNew")
-		for i := 0; i < len(inputNew); i++ {
-			if (i+1)%2 != 0 {
-				doubling := int(inputNew[i]) * 2
-				if doubling > 9 {
-					doubling = doubling - 9
-				}
-				fmt.Println(inputNew[i], doubling, "doubling")
-				stringResult += fmt.Sprint(doubling)
-			} else {
-				stringResult += string(inputNew[i])
-			}
-		}
 		sum := 0
+		auxMod := len(input) % 2
 
-		for i := 0; i < len(inputNew); i++ {
-			sum += int(inputNew[i])
+		for index, char := range strings.Split(input, "") {
+			digit, err := strconv.Atoi(char)
+			if err != nil {
+				return false
+			}
+
+			if auxMod == (index % 2) {
+				digit = digit * 2
+			}
+
+			if digit > 9 {
+				digit = digit - 9
+			}
+
+			sum += digit
 		}
 
-		if sum%10 == 0 {
+		if (sum % 10) == 0 {
 			status = true
 		}
-
 	}
 
 	return status
-}
-
-func getOnlyNumeric(s string) string {
-
-	reg, err := regexp.Compile("[^a-zA-Z]+")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return reg.ReplaceAllString(s, "")
-
 }
